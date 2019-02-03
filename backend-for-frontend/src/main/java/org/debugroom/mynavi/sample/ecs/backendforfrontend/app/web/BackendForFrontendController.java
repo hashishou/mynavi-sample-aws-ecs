36648765ece27c90cdc1e6ef8e3a221c3ce77ee7
Kohei Kawabata <kohei.kawabata@mac.com>
2019-02-03 21:09:59 +0900

    Change private subnet dns for application.yml and exclude unneeded import packages.

--- backend-for-frontend/src/main/java/org/debugroom/mynavi/sample/ecs/backendforfrontend/app/web/BackendForFrontendController.java
+++ backend-for-frontend/src/main/java/org/debugroom/mynavi/sample/ecs/backendforfrontend/app/web/BackendForFrontendController.java
@@ -1,16 +1,13 @@
 package org.debugroom.mynavi.sample.ecs.backendforfrontend.app.web;
 
-import org.debugroom.mynavi.sample.ecs.backendforfrontend.app.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestOperations;
 
-import javax.annotation.PostConstruct;
+import org.debugroom.mynavi.sample.ecs.backendforfrontend.app.model.User;
 
 @Controller
 public class BackendForFrontendController {
--- backend-for-frontend/src/main/java/org/debugroom/mynavi/sample/ecs/backendforfrontend/config/MvcConfig.java
+++ backend-for-frontend/src/main/java/org/debugroom/mynavi/sample/ecs/backendforfrontend/config/MvcConfig.java
@@ -6,9 +6,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.cbor.MappingJackson2CborHttpMessageConverter;
 import org.springframework.web.client.RestOperations;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
